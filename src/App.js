c9454474d31cc52fa531224693ccf2cbfe5c2861
brian <fil419@webmail.co.za>
2022-07-15 16:32:17 +0200

    index updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import './script.js';
+// import './script.js';
 
 function App() {
   return (
